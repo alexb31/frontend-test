5d274908b8235322789765fe06133aa741772462
alexb31 <alexandrebornstein@gmail.com>
2025-11-09 15:51:41 +0100

    Added Header component + Tailwind Config

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <header className="bg-background--header py-5">
+      <div className="mx-auto flex max-w-5xl flex-col gap-2 px-4 text-center">
+        <h1>Newsletters</h1>
+        <p>
+          Dans cette page, vous retrouvez l'ensemble des newsletters des
+          Echos et des marques satellites. Ainsi, vous pouvez découvrir toutes
+          nos newsletters selon vos centres d'intérêt et gérer plus
+          facilement l'inscription à vos newsletters.
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
