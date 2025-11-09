936dfc96d979e79dbf20430c7202d0bdaee3ce6c
alexb31 <alexandrebornstein@gmail.com>
2025-11-09 22:17:26 +0100

    Added try catch to api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import NewsLetterList from "@/components/NewsLetterList";
 import { USER_WITH_ONE_SUBSCRIPTION, USER_WITHOUT_SUBSCRIPTION, USER_WITH_MULTIPLE_SUBSCRIPTION } from "@/mocks/user";
 import { fetchNewsletters } from "@/api/newsletter";
+import { Newsletter } from "@/types";
 
 
 const page = async () => {
@@ -17,7 +18,13 @@ const page = async () => {
   const profileKey: keyof typeof USERS = "none"; // Simulation d'un utilisateur connecté
   const currentUser = USERS[profileKey];
 
-  const newsletters = await fetchNewsletters();
+  let newsletters: Newsletter[] = [];
+
+  try {
+    newsletters = await fetchNewsletters();
+  } catch (error) {
+    console.error("Failed to fetch newsletters:", error);
+  }
 
   return (
     <div className="container mx-auto mt-8">
